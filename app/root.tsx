c6f329d28f7fec7b0717f23b498777678b52ead2
Andrea Nanni <andrea.nanni3@gmail.com>
2023-04-26 00:03:45 +0200

    chore(deps): minor updates

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import {
   Link,
   Links,
@@ -7,42 +7,54 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
+import type { PropsWithChildren } from "react";
 import styles from "~/styles/global.css";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: V2_MetaFunction = () => [
+  {
+    title: "New Remix App",
+  },
+];
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
+    <Document>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">SSE</Link>
+            </li>
+            <li>
+              <Link to="/ai">AI</Link>
+            </li>
+            <li>
+              <Link to="/stream-ai">Stream AI</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
+    </Document>
+  );
+}
+
+function Document({ children }: PropsWithChildren) {
+  return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">SSE</Link>
-              </li>
-              <li>
-                <Link to="/ai">AI</Link>
-              </li>
-              <li>
-                <Link to="/stream-ai">Stream AI</Link>
-              </li>
-            </ul>
-          </nav>
-          <Outlet />
-        </div>
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -51,39 +63,26 @@ export default function App() {
   );
 }
 
-export function CatchBoundary() {
-  const caught = useCatch();
+export function ErrorBoundary() {
+  const error = useRouteError();
   return (
-    <html>
-      <head>
-        <title>Oops!</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <h1>
-          {caught.status} {caught.statusText}
-        </h1>
-        {caught.data?.message && <p>{caught.data.message}</p>}
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export function ErrorBoundary({ error }: { error: unknown }) {
-  console.error(error);
-  return (
-    <html>
-      <head>
-        <title>Oh no!</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <p>Something went wrong 😔</p>
-        <Scripts />
-      </body>
-    </html>
+    <Document>
+      <div>
+        {isRouteErrorResponse(error) ? (
+          <>
+            <p>Oh no! - Something went wrong! 😔</p>
+            <p>
+              {error.status}
+              {error.statusText && " - " + error.statusText}
+            </p>
+          </>
+        ) : (
+          <>
+            <p>Oops! - An unexpected error occured! 😱</p>
+            <p>{error instanceof Error ? error.message : "Unknown Error"}</p>
+          </>
+        )}
+      </div>
+    </Document>
   );
 }
--- remix.config.js
+++ remix.config.js
@@ -8,5 +8,8 @@ module.exports = {
   cacheDirectory: "node_modules/.cache",
   future: {
     v2_routeConvention: true,
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_meta: true,
   },
 };
